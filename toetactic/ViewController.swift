fadc6617848ed4d937b6b7beb0930774c9287b4d
Emerson <emersonmixrouse@gmail.com>
2023-05-03 10:36:21 -0600

    finished label

--- toetactic/ViewController.swift
+++ toetactic/ViewController.swift
@@ -31,39 +31,51 @@ class ViewController: UIViewController {
     @IBOutlet weak var winnerLabel: UILabel!
     //@IBOutlet weak var winnerLabel: UILabel!
 
-    @IBAction func buttonPressed(_ sender: UIButton) {
-
-        if gameState[sender.tag] == 0 {
-            gameState[sender.tag] = activePlayer
+    var log = 0
+    @IBAction func buttonPressed(_ sender: UIButton, _ tag: Int) {
+        print(x += 1)
+        print(sender.tag)
+        if gameState[tag] == 0 {
+            gameState[tag] = activePlayer
 
             if activePlayer == 1 {
-                sender.setImage(UIImage(named: "nought.png"), for: UIControl.State())
-                activePlayer = 2
+                sender.setImage(UIImage(named: "oimage2"), for: .normal)
+//                activePlayer = 2
             } else {
-                sender.setImage(UIImage(named: "cross.png"), for: UIControl.State())
-                activePlayer = 1
+                sender.setImage(UIImage(named: "ximage2"), for: .normal)
+//                activePlayer = 1
             }
         }
 
         for combination in winningCombinations {
-            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
-                if gameState[combination[0]] == 1 {
-                    winnerLabel.text = "Noughts has won!"
+            if gameState[combination[0]] == activePlayer && gameState[combination[1]] == activePlayer && gameState[combination[2]] == activePlayer {
+                if activePlayer == 1 {
+                    winnerLabel.text = "NOUGHTS HAS WON!"
                 } else {
-                    winnerLabel.text = "Crosses has won!"
+                    winnerLabel.text = "CROSSES HAS WON!"
                 }
 
                 disableButtons()
                 break
             }
+        
+//        button0.isEnabled = true
+//        x = 1
+//        button0.backgroundColor = .opaqueSeparator
+        //button0.setImage(UIImage(named: ""), for: .normal)
+            print("hi")
+       
         }
+        
+        activePlayer = activePlayer == 1 ? 2 : 1
 
         if !gameState.contains(0) {
-            winnerLabel.text = "It's a draw!"
+            winnerLabel.text = "IT'S A DRAW!"
         }
-
+        
+        sender.isEnabled = false
     }
-
+        
     func disableButtons() {
         button0.isEnabled = false
         button1.isEnabled = false
@@ -81,131 +93,143 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func button0(_ sender: Any) {
-        if x % 2 == 0 {
-            button0.setImage(UIImage(named: "oimage2"), for: .normal)
-        }else
-        {
-            button0.setImage(UIImage(named: "ximage2"), for: .normal)
-        }
+    @IBAction func button0(_ sender: UIButton) {
+//        if x % 2 == 0 {
+//            button0.setImage(UIImage(named: "oimage2"), for: .normal)
+//        }else
+//        {
+//            button0.setImage(UIImage(named: "ximage2"), for: .normal)
+//        }
+//
+//        x += 1
+        buttonPressed(sender,0)
         
-        x += 1
-        print("pressed! 0")
-        print(x)
+//        print("pressed! 0")
+//        print(x)
+    }
+    @IBAction func button1(_ sender: UIButton) {
+//        if x % 2 == 0 {
+//            button1.setImage(UIImage(named: "oimage2"), for: .normal)
+//        }else
+//        {
+//            button1.setImage(UIImage(named: "ximage2"), for: .normal)
+//        }
+//
+//        x += 1
+        buttonPressed(sender,1)
         
-        button0.isEnabled = false
+//        print("pressed! 0")
+//        print(x)
+    }
+    @IBAction func button2(_ sender: UIButton) {
+//        if x % 2 == 0 {
+//            button2.setImage(UIImage(named: "oimage2"), for: .normal)
+//        }else
+//        {
+//            button2.setImage(UIImage(named: "ximage2"), for: .normal)
+//        }
+//
+//        x += 1
+        buttonPressed(sender,2)
+        
+//        print("pressed! 0")
+//        print(x)
+    }
+    @IBAction func button3(_ sender: UIButton) {
+//        if x % 2 == 0 {
+//            button3.setImage(UIImage(named: "oimage2"), for: .normal)
+//        }else
+//        {
+//            button3.setImage(UIImage(named: "ximage2"), for: .normal)
+//        }
+//
+//        x += 1
+        buttonPressed(sender,3)
+        
+//        print("pressed! 0")
+//        print(x)
+//
+//        button3.isEnabled = false
+    }
+    @IBAction func button4(_ sender: UIButton) {
+//        if x % 2 == 0 {
+//            button4.setImage(UIImage(named: "oimage2"), for: .normal)
+//        }else
+//        {
+//            button4.setImage(UIImage(named: "ximage2"), for: .normal)
+//        }
+//
+//        x += 1
+        buttonPressed(sender,4)
+        
+//        print("pressed! 0")
+//        print(x)
+//
+//        button4.isEnabled = false
         
     }
-    @IBAction func button1(_ sender: Any) {
-        if x % 2 == 0 {
-            button1.setImage(UIImage(named: "oimage2"), for: .normal)
-        }else
-        {
-            button1.setImage(UIImage(named: "ximage2"), for: .normal)
-        }
+    @IBAction func button5(_ sender: UIButton) {
+//        if x % 2 == 0 {
+//            button5.setImage(UIImage(named: "oimage2"), for: .normal)
+//        }else
+//        {
+//            button5.setImage(UIImage(named: "ximage2"), for: .normal)
+//        }
+//
+//        x += 1
+        buttonPressed(sender,5)
         
-        x += 1
-        print("pressed! 1")
-        print(x)
-        
-        button1.isEnabled = false
+//        print("pressed! 0")
+//        print(x)
+//
+//        button5.isEnabled = false
         
     }
-    @IBAction func button2(_ sender: Any) {
-        if x % 2 == 0 {
-            button2.setImage(UIImage(named: "oimage2"), for: .normal)
-        }else
-        {
-            button2.setImage(UIImage(named: "ximage2"), for: .normal)
-        }
+    @IBAction func button6(_ sender: UIButton) {
+//        if x % 2 == 0 {
+//            button6.setImage(UIImage(named: "oimage2"), for: .normal)
+//        }else
+//        {
+//            button6.setImage(UIImage(named: "ximage2"), for: .normal)
+//        }
+//
+//        x += 1
+        buttonPressed(sender,6)
         
-        x += 1
-        print("pressed! 0")
-        print(x)
-        
-        button2.isEnabled = false
+//        print("pressed! 0")
+//        print(x)
+//
+//        button6.isEnabled = false
     }
-    @IBAction func button3(_ sender: Any) {
-        if x % 2 == 0 {
-            button3.setImage(UIImage(named: "oimage2"), for: .normal)
-        }else
-        {
-            button3.setImage(UIImage(named: "ximage2"), for: .normal)
-        }
-        
-        x += 1
-        print("pressed! 0")
-        print(x)
-        
-        button3.isEnabled = false
+    @IBAction func button7(_ sender: UIButton) {
+//        if x % 2 == 0 {
+//            button7.setImage(UIImage(named: "oimage2"), for: .normal)
+//        }else
+//        {
+//            button7.setImage(UIImage(named: "ximage2"), for: .normal)
+//        }
+//
+//        x += 1
+        buttonPressed(sender,7)
+//        print("pressed! 0")
+//        print(x)
+//
+//        button7.isEnabled = false
     }
-    @IBAction func button4(_ sender: Any) {
-        if x % 2 == 0 {
-            button4.setImage(UIImage(named: "oimage2"), for: .normal)
-        }else
-        {
-            button4.setImage(UIImage(named: "ximage2"), for: .normal)
-        }
-        
-        x += 1
-        print("pressed! 0")
-        print(x)
-        
-        button4.isEnabled = false    }
-    @IBAction func button5(_ sender: Any) {
-        if x % 2 == 0 {
-            button5.setImage(UIImage(named: "oimage2"), for: .normal)
-        }else
-        {
-            button5.setImage(UIImage(named: "ximage2"), for: .normal)
-        }
-        
-        x += 1
-        print("pressed! 0")
-        print(x)
-        
-        button5.isEnabled = false    }
-    @IBAction func button6(_ sender: Any) {
-        if x % 2 == 0 {
-            button6.setImage(UIImage(named: "oimage2"), for: .normal)
-        }else
-        {
-            button6.setImage(UIImage(named: "ximage2"), for: .normal)
-        }
-        
-        x += 1
-        print("pressed! 0")
-        print(x)
-        
-        button6.isEnabled = false
-    }
-    @IBAction func button7(_ sender: Any) {
-        if x % 2 == 0 {
-            button7.setImage(UIImage(named: "oimage2"), for: .normal)
-        }else
-        {
-            button7.setImage(UIImage(named: "ximage2"), for: .normal)
-        }
-        
-        x += 1
-        print("pressed! 0")
-        print(x)
-        
-        button7.isEnabled = false
-    }
-    @IBAction func button8(_ sender: Any) {
-        if x % 2 == 0 {
-            button8.setImage(UIImage(named: "oimage2"), for: .normal)
-        }else
-        {
-            button8.setImage(UIImage(named: "ximage2"), for: .normal)
-        }
-        
-        x += 1
-        print("pressed! 0")
-        print(x)
-        
-        button8.isEnabled = false
+    @IBAction func button8(_ sender: UIButton) {
+//        if x % 2 == 0 {
+//            button8.setImage(UIImage(named: "oimage2"), for: .normal)
+//        }else
+//        {
+//            button8.setImage(UIImage(named: "ximage2"), for: .normal)
+//        }
+//
+//        x += 1
+        buttonPressed(sender,8)
+//        print("pressed! 0")
+//        print(x)
+//
+//        button8.isEnabled = false
     }
     
   
